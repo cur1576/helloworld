3a1c16463fb96e9dae0fcb8c8532df2865d4359f
cur1576 <cur1576@gmx.de>
2019-02-06 14:18:33 +0100

    test23

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // test 2
+        // test 2 3
     }
 }
